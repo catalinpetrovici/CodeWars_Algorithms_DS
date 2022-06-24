c593caffe66e376fe9993040bb5402877fefc852
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-24 19:45:59 +0300

    Having fun with JavaScript Decorators

--- FrontEndMasters/A Practical Guide to Algorithms with JavaScript/Caching, Memoization/memoizationBasic.js
+++ FrontEndMasters/A Practical Guide to Algorithms with JavaScript/Caching, Memoization/memoizationBasic.js
@@ -24,13 +24,44 @@ const memoTimes10 = (n) => {
   return result;
 };
 
-console.log('\n');
-console.log(`~~~~~~~~~~~~Task 2~~~~~~~~~~~~`);
-console.time('Task 2 calculated execution time');
-console.log(`Task 2 calculated value:`, memoTimes10(9)); // calculated
-console.timeEnd('Task 2 calculated execution time');
+// console.log('\n');
+// console.log(`~~~~~~~~~~~~Task 2~~~~~~~~~~~~`);
+// console.time('Task 2 calculated execution time');
+// console.log(`Task 2 calculated value:`, memoTimes10(9)); // calculated
+// console.timeEnd('Task 2 calculated execution time');
 
-console.log('\n');
-console.time('Task 2 cached execution time');
-console.log(`Task 2 cached value:`, memoTimes10(9)); // cached
-console.timeEnd('Task 2 cached execution time');
+// console.log('\n');
+// console.time('Task 2 cached execution time');
+// console.log(`Task 2 cached value:`, memoTimes10(9)); // cached
+// console.timeEnd('Task 2 cached execution time');
+
+// #### Decorators
+
+function loggingDecorator(wrapperFn, y) {
+  let task = y;
+  return {
+    store: function store(x) {
+      const cached = cache[x] ? 'cached' : 'calculated';
+
+      console.log(`~~~~~~~~~~~~Task ${task}~~~~~~~~~~~~`);
+      console.time(`Task ${task} ${cached} execution time`);
+      const result = wrapperFn(x);
+      console.log(`Task ${task} ${cached} value:`, result);
+      console.timeEnd(`Task ${task} ${cached} execution time`);
+
+      task++;
+    },
+
+    getTask: function getTask() {
+      console.log(task);
+      return task;
+    },
+  };
+}
+
+const wrappedCached = loggingDecorator(memoTimes10, 2);
+
+wrappedCached.store(9);
+wrappedCached.store(9);
+wrappedCached.store(9);
+wrappedCached.store(10);
--- FrontEndMasters/A Practical Guide to Algorithms with JavaScript/Caching, Memoization/memoizationClosure.js
+++ FrontEndMasters/A Practical Guide to Algorithms with JavaScript/Caching, Memoization/memoizationClosure.js
@@ -18,21 +18,75 @@ const memoizedClosureTimesM = (m) => {
 
 const memoClosureTimes10 = memoizedClosureTimesM(10);
 
-console.log(`~~~~~~~~~~~~Task 3~~~~~~~~~~~~`);
-console.log('\n');
-console.time('Task 3 calculated execution time');
-console.log(`Task 3 calculated value:`, memoClosureTimes10(9)); // calculated
-console.timeEnd('Task 3 calculated execution time');
+// console.log(`~~~~~~~~~~~~Task 3~~~~~~~~~~~~`);
+// console.log('\n');
+// console.time('Task 3 calculated execution time');
+// console.log(`Task 3 calculated value:`, memoClosureTimes10(9)); // calculated
+// console.timeEnd('Task 3 calculated execution time');
 
-console.log('\n');
-console.time('Task 3 cached execution time');
-console.log(`Task 3 cached value:`, memoClosureTimes10(9)); // cached
-console.timeEnd('Task 3 cached execution time');
+// console.log('\n');
+// console.time('Task 3 cached execution time');
+// console.log(`Task 3 cached value:`, memoClosureTimes10(9)); // cached
+// console.timeEnd('Task 3 cached execution time');
 
-console.log('\n');
-console.log(`Task 4 calculated value:`, memoClosureTimes10(7)); // cached
-console.log(`Task 4 cached value:`, memoClosureTimes10(7)); // cached
+// console.log('\n');
+// console.log(`Task 4 calculated value:`, memoClosureTimes10(7)); // cached
+// console.log(`Task 4 cached value:`, memoClosureTimes10(7)); // cached
 
-console.log('\n');
-console.log(`Task 5 calculated value:`, memoClosureTimes10(8)); // cached
-console.log(`Task 5 cached value:`, memoClosureTimes10(8)); // cached
+// console.log('\n');
+// console.log(`Task 5 calculated value:`, memoClosureTimes10(8)); // cached
+// console.log(`Task 5 cached value:`, memoClosureTimes10(8)); // cached
+
+// ######################################################################
+
+const memoizedClosureTimesMAccessor = (m) => {
+  let cache = {};
+  return {
+    store: function store(n) {
+      if (cache[n]) {
+        console.log(`Fetching from cache:`, n);
+        return cache[n];
+      }
+
+      let result = n * m;
+      console.log(`Calculating result:`, result);
+      cache[n] = result;
+      return result;
+    },
+    getCache: function getCache() {
+      return cache;
+    },
+  };
+};
+
+const memoClosureTimes10Accessor = memoizedClosureTimesMAccessor(10);
+
+function loggingDecorator(wrapperFn, y) {
+  let task = y;
+  return {
+    store: function store(x) {
+      const cache = wrapperFn.getCache();
+      const cached = cache[x] ? 'cached' : 'calculated';
+
+      console.log(`~~~~~~~~~~~~Task ${task}~~~~~~~~~~~~`);
+      console.time(`Task ${task} ${cached} execution time`);
+      const result = wrapperFn.store(x);
+      console.log(`Task ${task} ${cached} value:`, result);
+      console.timeEnd(`Task ${task} ${cached} execution time`);
+
+      task++;
+    },
+
+    getTask: function getTask() {
+      console.log(task);
+      return task;
+    },
+  };
+}
+
+const wrappedCached = loggingDecorator(memoClosureTimes10Accessor, 1);
+
+wrappedCached.store(9);
+wrappedCached.store(9);
+wrappedCached.store(12);
+wrappedCached.store(12);
