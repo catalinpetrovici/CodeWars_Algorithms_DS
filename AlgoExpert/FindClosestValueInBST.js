ba4e2253aa2cb627ff317e5c64a89aa64f838738
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-28 20:24:46 +0300

    Daily Coding - 39 - Closest Binary Search Tree Value

---
+++ AlgoExpert/FindClosestValueInBST.js
@@ -0,0 +1,98 @@
+// Finding the Closest Element in a Binary Search Tree
+
+// We are given a binary search tree and a target, and we are tasked with finding a value in the binary search tree that is closest to value of the given target. In this problem, there will only be one closest value.
+
+//                10
+//         5              15
+//     2       5      13      22
+// 1                      14
+
+const data = {
+  number: 10,
+  greater: {
+    number: 15,
+    greater: { number: 22, greater: {}, less: {} },
+    less: {
+      number: 13,
+      greater: { number: 14, greater: {}, less: {} },
+      less: {},
+    },
+  },
+  less: {
+    number: 5,
+    greater: { number: 5, greater: {}, less: {} },
+    less: {
+      number: 2,
+      greater: {},
+      less: { number: 1, greater: {}, less: {} },
+    },
+  },
+};
+
+// Complexity Analysis
+// Time Complexity: Avg: O(log N) / Worst: O(N)
+// Space Complexity: Avg: O(log N) / Worst: O(N)
+
+const findTheClosestElementInABinarySearchTree = (tree, target) => {
+  return findClosestValueInBstHelper(tree, target, tree.number);
+};
+
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (!tree) return closest;
+
+  if (Math.abs(target - closest) > Math.abs(target - tree.number)) {
+    closest = tree.number;
+  }
+
+  if (target < tree.number) {
+    // console.log('left');
+    return findClosestValueInBstHelper(tree.less, target, closest);
+  }
+
+  if (target > tree.number) {
+    // console.log('right');
+    return findClosestValueInBstHelper(tree.greater, target, closest);
+  }
+
+  return closest;
+}
+
+// console.log(findTheClosestElementInABinarySearchTree(data, 12));
+// console.log(findTheClosestElementInABinarySearchTree(data, 3));
+
+// ##################################################################################
+
+// Iterative
+
+// Complexity Analysis
+// Time Complexity: Avg: O(log N) / Worst: O(N)
+// Space Complexity: Avg: O(1) / Worst: O(1)
+
+const findTheClosestElementInABinarySearchTreeIterative = (tree, target) => {
+  return findClosestValueInBstHelperIterative(tree, target, tree.number);
+};
+
+function findClosestValueInBstHelperIterative(tree, target, closest) {
+  currentNode = tree;
+
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.number)) {
+      closest = currentNode.number;
+    }
+
+    if (target < currentNode.number) {
+      console.log('left');
+      currentNode = currentNode.less;
+    } else if (target > currentNode.number) {
+      console.log('right');
+      currentNode = currentNode.greater;
+    } else {
+      break;
+    }
+  }
+
+  return closest;
+}
+
+console.log(findTheClosestElementInABinarySearchTreeIterative(data, 12));
+console.log(findTheClosestElementInABinarySearchTreeIterative(data, 3));
