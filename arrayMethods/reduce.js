534c7f08f9b51b283eaf827b4a8cde8c78087126
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-06-28 19:07:35 +0300

    Array Methods - Reduce - Objects

--- arrayMethods/reduce.js
+++ arrayMethods/reduce.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const people = [
   { name: 'Catalin Pe', cubes: 12 },
   { name: 'Beatrice An', cubes: 9 },
@@ -36,3 +38,43 @@ const averageCubes =
   people.reduce((acc, curr) => acc + curr.cubes, 0) / people.length;
 
 // => 10.5
+
+// ?## Objects
+
+import { cart } from './someDummyData/data.mjs';
+
+// let {totalItems, cartTotal} = cart.reduce(
+let total = cart.reduce(
+  (total, cartItem) => {
+    const { amount, price } = cartItem;
+    // count items
+    total.totalItems += amount;
+    // calculate total sum
+    total.cartTotal += amount * price;
+    return total;
+  },
+  { totalItems: 0, cartTotal: 0 }
+);
+
+// cartTotal = parseFloat(cartTotal.toFixed(2))
+// console.log(totalItems, cartTotal);
+// console.log(total);
+
+const url = `https://api.github.com/users/catalinpetrovici24/repos?per_page=100`;
+
+const fetchRepos = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const newData = data.reduce((total, repo) => {
+    const { language } = repo;
+
+    if (language) {
+      // if exist return + 1 // if not return 1
+      total[language] = total[language] + 1 || 1;
+    }
+    return total;
+  }, {});
+  console.log(newData);
+};
+fetchRepos();
---
+++ arrayMethods/someDummyData/data.mjs
@@ -0,0 +1,26 @@
+export const cart = [
+  {
+    title: 'Viata 3.0. Omul in epoca inteligentei artificiale',
+    author: 'MAX TEGMARK',
+    price: 33.6,
+    amount: 1,
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'JAMES CLEAR',
+    price: 41.3,
+    amount: 1,
+  },
+  {
+    title: 'Dopamina',
+    author: 'DANIEL Z. LIEBERMAN, MICHAEL E. LONG',
+    price: 38.5,
+    amount: 1,
+  },
+  {
+    title: 'Dune',
+    author: 'FRANK HERBERT',
+    price: 45.49,
+    amount: 1,
+  },
+];
